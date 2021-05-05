55e7673e4001cb3405556ddd5a91ee6a72376bb4
Lucas Mateus <lukinhasm07@outlook.com>
2021-05-05 16:23:36 -0300

    Adicionando repositorios spring e react ao projeto

---
+++ backend/src/main/java/com/projetospringreact/vendasspring/VendasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projetospringreact.vendasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetospringreact/vendasspring/VendasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringreact.vendasspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
